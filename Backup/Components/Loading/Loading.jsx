8330dc9b85da9f068bd8c7c39c1fd273c30a8e69
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-01-08 07:01:53 +0330

    Delete Backup directory

--- Backup/Components/Loading/Loading.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import { HashLoader } from "react-spinners";
-export default function loading() {
-  return <HashLoader color="#fff" size={30} />;
-}
--- Backup/Services/FetchApi.js
+++
@@ -1,9 +0,0 @@
-export async function fetchApi(url) {
-  try {
-    const fetchURL = await fetch(url);
-    if (!fetchURL.ok) throw new Error("Something Went Wrong !!");
-    return await fetchURL.json();
-  } catch (error) {
-    console.log(error);
-  }
-}
