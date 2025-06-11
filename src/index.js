d86e672dee0658574036d8b453819cfc16e693d9
Amirreza Riahi <amirreza.riahi3311@gmail.com>
2025-06-11 16:27:46 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./Components/Main.jsx";
 import { BrowserRouter } from "react-router-dom";
-//import css
 import "./css/style.css";
 ReactDOM.render(
   <BrowserRouter>
